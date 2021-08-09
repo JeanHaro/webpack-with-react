9f4952ee43043235e09194d1484e577f5f6b39f6
Jean Haro <JeanCHL98@hotmail.com>
2021-08-08 19:58:50 -0500

    Instalación y configuración de React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// Mostrar dentro de un archivo HTML nuestra aplicación
+ReactDOM.render(<App />, document.getElementById('app'));
